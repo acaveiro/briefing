70250f3d77dc2f3551d7b5ae026ee8ad889aee8a
csdim <69046188+csdim@users.noreply.github.com>
2020-10-22 12:23:31 +0100

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -3,6 +3,36 @@
     $('input[type="checkbox"]').not(this).prop('checked', false);      
 });
 
+
+window.onload = function () {
+
+	var chart = new CanvasJS.Chart("chartContainer", {
+		animationEnabled: true,
+		theme: "light2",
+		title:{
+			text: "Simple Line Chart"
+		},
+		data: [{        
+			type: "line",
+			  indexLabelFontSize: 16,
+			dataPoints: [
+				{ y: 450 },
+				{ y: 414},
+				{ y: 520, indexLabel: "\u2191 highest",markerColor: "red", markerType: "triangle" },
+				{ y: 460 },
+				{ y: 450 },
+				{ y: 500 },
+				{ y: 480 },
+				{ y: 480 },
+				{ y: 410 , indexLabel: "\u2193 lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
+				{ y: 500 },
+				{ y: 480 },
+				{ y: 510 }
+			]
+		}]
+	});
+chart.render();
+
 var now = new Date();
 var utc = new Date(now.getTime() + now.getTimezoneOffset() * 60000 + 1800000);
 var utcFuture = new Date(now.getTime() + now.getTimezoneOffset() * 60000 + 1800000 + 3600000 );
